4db6048a9e2c870b75d820724a6f8d6001dbc270
SALMAN2915 <138514798+SALMAN2915@users.noreply.github.com>
2025-05-22 15:45:58 +0530

    Add files via upload

---
+++ 02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; // treate all js code as a newer version
+
+// alert("Hello") // we are using nodejs, not browser
+
+console.log( 3 + 3 )
+
+console.log("Salman")
+
+//code readability should be high
+
+
+let name = "Salman"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standaLone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "salman");
+
+
+console.log(typeof age);
+
+console.log(typeof null);
+
+console.log(typeof undefined);
