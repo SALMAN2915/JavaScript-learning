542d8f7cd6c5fd950ffc3a11e00d80197ddd33fb
SALMAN2915 <138514798+SALMAN2915@users.noreply.github.com>
2025-05-23 14:07:47 +0530

    Add files via upload

---
+++ datatypes-summary.js
@@ -0,0 +1,43 @@
+// Primitive
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id == anotherId);
+
+// const bigNumber = 34350613411445n
+
+
+
+// Refrence (Non Primritive)
+
+// Array, Objects, Functions    
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj ={
+    name: "Salman",
+    age: 22,
+}
+
+const myfunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+
+
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack (Primitive), Heap (Non-Primitive)
